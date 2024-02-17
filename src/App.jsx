201d08ca04ce42763340b801118acf579263cd12
Rigoberto Chimalhua <webmaster25@outlook.es>
2024-02-16 23:16:06 -0600

    Se creó nuevo repositorio en github y se subión la primera versión de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,14 +7,18 @@ import Products from './components/Products'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { IS_DEVELOPMENT } from './config'
+import { FiltersContext } from './context/filters'
 
 //Custom Hook
 function useFilters(){
+  const { filters, setFilters } = useContext(FiltersContext)
 
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+  // const [filters, setFilters] = useState({
+  //   category: 'all',
+  //   minPrice: 0
+  // })
+
+  //const filters = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
